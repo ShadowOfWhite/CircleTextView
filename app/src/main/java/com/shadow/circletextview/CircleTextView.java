9b202785a97cab1c1776064f0c86faf9eebf14ae
shadow <lin1_yang@163.com>
2019-08-28 23:02:42 +0800

    init project

---
+++ app/src/main/java/com/shadow/circletextview/CircleTextView.java
@@ -0,0 +1,19 @@
+package com.shadow.circletextview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CircleTextView extends View {
+    public CircleTextView(Context context) {
+        super(context);
+    }
+
+    public CircleTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
